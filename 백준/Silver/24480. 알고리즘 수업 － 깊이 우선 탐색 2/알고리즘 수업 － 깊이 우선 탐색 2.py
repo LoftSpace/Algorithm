19672283aa56c2b9047c49300fbabef5dc412067
Yongjun <128569903+LoftSpace@users.noreply.github.com>
2024-11-21 20:05:36 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1376 ms, Memory: 728632 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**5)
+N,M,R = map(int,input().split())
+G = [[] for i in range(N+1)]
+visited=[0]*(N+1)
+#print(G)
+for i in range(M):
+    u,v=map(int,input().split())
+    G[u].append(v)
+    G[v].append(u)
+for i in range(N+1):
+    G[i].sort()
+    G[i].reverse()
+#print(G)
+count=0
+def DFS_Visit(i):
+    global count
+    count+=1
+    visited[i]=count
+    for j in G[i]:
+        #print("%d->%d"%(i,j))
+        if visited[j]==0:
+            DFS_Visit(j)
+    
+
+def DFS(R):
+    DFS_Visit(R)
+
+DFS(R)
+for i in range(1,N+1):
+    print(visited[i])
