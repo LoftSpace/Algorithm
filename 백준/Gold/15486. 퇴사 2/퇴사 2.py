f56b572522ebb215b3d3ca93a4186a19094b5024
Yongjun <128569903+LoftSpace@users.noreply.github.com>
2024-12-20 15:23:26 +0900

    [Gold V] Title: 퇴사 2, Time: 396 ms, Memory: 122720 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0] * (N+1)
+
+for i in range(1,N + 1):
+    t, p = map(int,input().split())
+    dp[i] = max(dp[i-1],dp[i])
+    if i + t <= N + 1 :
+        dp[i + t -1] = max(dp[i + t - 1] , dp[i - 1] + p)
+
+print(dp[-1])
