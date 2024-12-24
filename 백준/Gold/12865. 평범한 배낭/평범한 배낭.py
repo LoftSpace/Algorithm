94a95c5437b12c8939ee382f167a00897c575dc9
Yongjun <128569903+LoftSpace@users.noreply.github.com>
2024-12-24 22:11:56 +0900

    [Gold V] Title: 평범한 배낭, Time: 296 ms, Memory: 188728 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,25 +1,23 @@
 import sys
-input=sys.stdin.readline
 
-num,total_weight = map(int,input().split())
-weight=[0]
-value=[0]
-dp=[[0]*(total_weight+1) for _ in range(num+1)]
-for i in range(num):
-    w,v=map(int,input().split())
-    weight.append(w)
-    value.append(v)
+input = sys.stdin.readline
 
-for i in range(num+1):
-    dp[i][0]=0
+N , K = map(int,input().split())
 
-for i in range(total_weight):
-    dp[0][i]=0
+W = [0]
+V = [0]
+dp = [[0] * (K + 1) for _ in range( N + 1 )]
 
-for i in range(1,num+1):
-    for j in range(1,total_weight+1):
-        if j < weight[i]:
-            dp[i][j]=dp[i-1][j]
-        else:
-            dp[i][j]=max(dp[i-1][j-weight[i]]+value[i],dp[i-1][j])
-print(dp[num][total_weight])
+for i in range(N):
+    w , v = map(int,input().split())
+    W.append(w)
+    V.append(v)
+
+for i in range(1, N + 1) :
+    for w in range(1,K + 1):
+        if W[i] <= w : # 전체 용량 초과 안한다면
+                dp[i][w] = max(dp[i-1][w],dp[i-1][w-W[i]] + V[i])
+        else :
+            dp[i][w] = dp[i-1][w]
+
+print(dp[N][K])
