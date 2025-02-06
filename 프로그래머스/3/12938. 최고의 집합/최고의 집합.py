4d8cc1459be130c10cee670c738903a13ce28f8c
Yongjun <128569903+LoftSpace@users.noreply.github.com>
2025-02-06 13:36:38 +0900

    [level 3] Title: 최고의 집합, Time: 1.68 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -0,0 +1,20 @@
+def solution(n, s):
+    if s < n :
+        return [-1]
+    elements = [0] * n
+    a = s // n
+    for i in range(n):
+        elements[i] = a
+    remain = s % n
+    i = 0
+    while remain > 0 :
+        elements[-1-i] += 1
+        i += 1
+        remain -=1
+  
+    for i in elements :
+        if i == 0 :
+            return [-1]
+        
+    answer = elements
+    return answer
