c798a5f3d38178258b63413535ae2f8adfd9f8ef
Yongjun <128569903+LoftSpace@users.noreply.github.com>
2025-02-13 16:32:58 +0900

    [Silver II] Title: 연속합, Time: 112 ms, Memory: 121644 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,12 @@
+import sys
+input= sys.stdin.readline
+
+n=int(input())
+l=list(map(int,input().split()))
+
+dp=[0]*n
+
+dp[0]=l[0]
+for i in range(1,n):
+    dp[i]=max(l[i],dp[i-1]+l[i])
+print(max(dp))
