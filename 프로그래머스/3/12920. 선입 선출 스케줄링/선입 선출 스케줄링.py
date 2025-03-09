9227c7f27565dbf2e536c8080533b1d6bd378c16
Yongjun <128569903+LoftSpace@users.noreply.github.com>
2025-03-10 01:34:21 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 12.55 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.py
@@ -0,0 +1,21 @@
+def solution(n, cores):
+    if n<=len(cores):
+        return n
+    n-=len(cores)
+    left, right=1, max(cores)*n
+    while left<right:
+        mid=(left+right)//2
+        proc=0
+        for core in cores:
+            proc+=mid//core
+        if proc>=n:
+            right=mid
+        else:
+            left=mid+1
+    for core in cores:
+        n-=(right-1)//core
+    for i in range(len(cores)):
+        if right%cores[i]==0:
+            n-=1
+            if n==0:
+                return i+1
