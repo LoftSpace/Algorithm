2f685506a774bf3343bdf98927a04c3ab7ce95b3
Yongjun <128569903+LoftSpace@users.noreply.github.com>
2025-03-13 02:09:56 +0900

    [Gold III] Title: 파일 합치기, Time: 1880 ms, Memory: 118448 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -7,23 +7,18 @@ for test in range(T):
 
     N = int(input())
     arr = list(map(int,input().split()))
-    dp = [[1000000000] * (N+1) for _ in range(N+1)]
+    arrSum = [0] * ( N + 1)
+    for i in range(N):
+        arrSum[i + 1] = arrSum[i] + arr[i]
 
-    sumArr = [0] * (N+1)
-
+    dp = [[1000000000] * (N + 1) for _ in range(N + 1)]
     for i in range(1,N + 1):
-        sumArr[i] = sumArr[i-1] + arr[i-1]
-
-    for i in range(1, N+ 1) :
         dp[i][i] = 0
-
-
-
-    for n in range(1,N):
-        for i in range(1, N-n + 1):
-            j = i + n
-            for k in range(i,j):
-                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + sumArr[j] - sumArr[i - 1])
-                
-
-    print(dp[1][N])
+    for k in range(1,N):
+        for i in range(1,N - k + 1):
+            j = i + k
+            for a in range(i,j):
+                dp[i][j] = min(dp[i][j],dp[i][a] + dp[a + 1][j] + arrSum[j] - arrSum[i-1])
+    
+    
+    print(dp[1][N ])
