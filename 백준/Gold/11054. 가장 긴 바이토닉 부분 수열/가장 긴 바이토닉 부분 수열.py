d18bdea3bfc8f302973b90205797e0a0740f8461
Yongjun <128569903+LoftSpace@users.noreply.github.com>
2025-03-15 01:33:52 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 576 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,24 +1,47 @@
 import sys
-input= sys.stdin.readline
 
-n=int(input())
+input = sys.stdin.readline
 
-A=list(map(int,input().split()))
+N = int(input())
+Arr = list(map(int,input().split()))
 
-dp=[[1,0] for _ in range(n)]
+answer = 1
 
-for i in range(n):
-    for j in range(i,-1,-1):
-        if A[j] < A[i] :
-            dp[i][0]= max(dp[i][0],dp[j][0]+1)
+def LIS(arr):
+    lis = []
+    lis.append(arr[0])
+    n = len(arr)
+    for i in range(1,n) :
+        if arr[i] > lis[-1]:
+            lis.append(arr[i])
+        else :
+            left = 0
+            right = len(lis) - 1
 
-for i in range(n,-1,-1):
-    for j in range(i,n):
-        if A[j] < A[i] :
-            dp[i][1]=max(dp[i][1],dp[j][1]+1)
-max=0
-for i in range(n):
-    if max < dp[i][0]+dp[i][1]:
-        max=dp[i][0]+dp[i][1]
-print(max)
-#print(dp)
+            while left <= right :
+                mid = (left + right) // 2
+                if lis[mid] == arr[i] :
+                    left = mid
+                    break
+                elif lis[mid] > arr[i] :
+                    right = mid - 1
+                else :
+                    left = mid + 1
+            lis[left] = arr[i]
+    return len(lis)
+
+reversed = Arr[::-1]
+
+for i in range(N):
+    left = 0
+    right = 0
+    if len(Arr[:i + 1]) > 0 :
+        left = LIS(Arr[:i + 1])
+    temp = Arr[i:]
+    temp = temp[::-1]
+    if len(temp) > 0 :
+        right = LIS(temp)
+    #print(left,right)
+    answer = max(left+right-1,answer)
+   
+print(answer)
